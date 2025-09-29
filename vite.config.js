73c05ffab58a0cd15b88e3be6b9c7e8d3a671966
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-09-29 19:37:56 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
     watch: {
       usePolling: true
     },
-    allowedHosts: ['admin.flavorforge.io'],
+    allowedHosts: ['admin.flavorforge.io','www.flavorforge.io'],
   },
 });
